e7f557b4edc505989e3a86bfa8298a495c015851
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-06-05 01:09:41 -0700

    Add classes.

--- src/TemplateRouter.php
+++ src/TemplateRouter.php
@@ -1,20 +1,26 @@
 <?php 
 
-/**
- * Given the location of template files, this router assumes
- *  a valid route if the request URI matches the name of a template file.
- */
 
 
-function processRoute($request) {
-    global $routes;
-    ob_start();
-    $requestString = substr($request, 1);
-    $callback = function() { return "Load the template file here..."; };
-    $output = $callback();
 
-    $out = ob_get_contents();
-    ob_end_clean();
-    return $out;
+class TemplateRouter extends Router {
+    /**
+     * Given the location of template files, this router assumes
+     *  a valid route if the request URI matches the name of a template file.
+     */
+
+
+    function processRoute($request) {
+        global $routes;
+        ob_start();
+        $requestString = substr($request, 1);
+        $callback = function() { return "Load the template file here..."; };
+        $output = $callback();
+
+        $out = ob_get_contents();
+        ob_end_clean();
+        return $out;
+
+    }
 
 }
